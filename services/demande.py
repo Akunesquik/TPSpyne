92630981831a3f65e48451a5eb724a17de1bd791
Mehdi <78212042+m-aissi@users.noreply.github.com>
2023-10-04 15:06:53 +0200

    ajout class demande

---
+++ services/demande.py
@@ -0,0 +1,11 @@
+class Demande:
+    def __init__(self, nom, adresse, email, telephone, montant_pret, duree_pret, description_propriete, revenu_mensuel, depenses_mensuelles):
+        self.nom = nom
+        self.adresse = adresse
+        self.email = email
+        self.telephone = telephone
+        self.montant_pret = montant_pret
+        self.duree_pret = duree_pret
+        self.description_propriete = description_propriete
+        self.revenu_mensuel = revenu_mensuel
+        self.depenses_mensuelles = depenses_mensuelles
--- services/service_extraction_infos_metier.py
+++ services/service_extraction_infos_metier.py
@@ -1,8 +1,16 @@
 from spyne import Application, rpc, ServiceBase, Unicode
 from spyne.protocol.soap import Soap11
-
+from demande import Demande
 class ServiceExtractionInfosMetier(ServiceBase):
     @rpc(Unicode, _returns=Unicode)
     def pretraitement_de_texte(ctx, texte):
-        # Implémentez le prétraitement du texte ici
+        # on prend le texte et a chaque ligne on prend tous les elements a droite de : c'est à adire element:11 on prend 11 
+        texte = texte.split("\n")
+        for i in range(len(texte)):
+            texte[i] = texte[i].split(":")[1]
+        
+        # on affiche le texte pour chaque ligne
+        for i in range(len(texte)):
+            print(texte[i]) 
+        
         return texte
