e1769d144c2e890683a940993a06d34c82c5d048
Mehdi <78212042+m-aissi@users.noreply.github.com>
2023-10-05 16:11:45 +0200

    update parser and squeleton of services

--- services/service_evaluation_propriete.py
+++ services/service_evaluation_propriete.py
@@ -1,15 +1,34 @@
 from spyne import Application, rpc, ServiceBase, Unicode
 from spyne.protocol.soap import Soap11
+from demande import Demande
 
 class ServiceEvaluationPropriete(ServiceBase):
     @rpc(Unicode, _returns=Unicode)
     def analyse_des_donnees_du_marche_immobilier(ctx, donnees):
-        # Implémentez l'analyse des données du marché immobilier ici
+        #si la propriété est en ile-de-france on ajoute 10 000 au prix
+        
+        #on cree un tableau qui stock les departements d'ile-de-france
+        departements = ["75", "77", "78", "91", "92", "93", "94", "95"]
+        #on regarde dans adresse le moment ou il y a une suite de 5 chiffres et on compare les deux premiers avec departements  
+        for i in range(len(donnees.adresse)-5):
+            if donnees.adresse[i:i+5].isdigit():
+                if donnees.adresse[i:i+2] in departements:
+                    donnees.montant_pret += 10000
+        
         return donnees
 
     @rpc(Unicode, _returns=Unicode)
     def inspection_virtuelle_ou_sur_place(ctx, propriete):
-        # Implémentez l'inspection virtuelle ou sur place ici
+        # on met un bonus ou un malus aléatoire de 1 à 5000 euro en fonction de si la propriété est bien entretenue ou non
+        import random
+        bonus = random.randint(1, 5000)
+        
+        #si le texte contient bien entretenue 
+        if "bien entretenue" in propriete.description_propriete:
+            propriete.montant_pret += bonus
+        else:
+            propriete.montant_pret -= bonus
+        
         return propriete
 
     @rpc(Unicode, _returns=Unicode)
